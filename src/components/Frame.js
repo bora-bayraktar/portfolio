e445ee843e0d5d29c6acb54b4de349233d1201d0
Kemal Bora Bayraktar <78881290+bora-bayraktar@users.noreply.github.com>
2022-02-01 18:28:19 +0300

    Increase responsivness

---
+++ src/components/Frame.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Frame extends Component {
+    render() {
+        return (
+            <div className="frame">
+                <a href="mailto:kemalborabayraktar@gmail.com" id="mail">
+                    kemalborabayraktar@gmail.com
+                </a>
+                <p id="location">Istanbul/Turkey</p>
+            </div>
+        );
+    }
+}
+
+export default Frame;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import Hello from "./Hello";
+import Frame from "./Frame";
 
 class Header extends Component {
     render() {
         return (
             <div className="header">
+                <Frame />
                 <Hello />
             </div>
         );
