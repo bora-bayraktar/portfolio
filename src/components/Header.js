53976a6a25e71013f7d2361fe52074a66e64b8b1
Kemal Bora Bayraktar <78881290+bora-bayraktar@users.noreply.github.com>
2022-02-01 03:22:44 +0300

    Add name and majors

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 import Hello from "./Hello";
 
 class Header extends Component {
-  render() {
-    return (
-      <div className="header">
-        <Hello />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="header">
+                <Hello />
+            </div>
+        );
+    }
 }
 
 export default Header;
