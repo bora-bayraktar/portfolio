04905b58b14c084fb6a193f5e09bb0d2e62621cd
Kemal Bora Bayraktar <78881290+bora-bayraktar@users.noreply.github.com>
2022-02-02 16:07:34 +0300

    Add fa icons

--- src/components/MoreAboutMeButton.js
+++ src/components/MoreAboutMeButton.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class MoreAboutMeButton extends Component {
     render() {
         return (
-            <div class="more-about-button">
+            <div className="more-about-button">
                 <ul id="more-button-ul">
                     <li id="more-button-li">
                         <span id="more-button-span">More About Me</span>
